6b087ba1dcec25e138153e6083b193ac7051c13a
Daniel Rodrigues <capau.2020120iipi0064@aluno.ifpi.edu.br>
2024-06-22 14:05:19 -0300

    Agora pode rodar em linux e windows

--- utils/abrir_imagem_sistemas.py
+++ utils/abrir_imagem_sistemas.py
@@ -1,21 +1,5 @@
-import subprocess
-import sys
-import os
+from PIL import Image
 
-def abrir_imagem(image_path):
-    try:
-        if sys.platform.startswith('darwin'):
-            subprocess.call(('open', image_path))
-        elif os.name == 'nt':
-            subprocess.call(('explorer', image_path))
-        elif os.name == 'posix':
-            subprocess.call(('xdg-open', image_path))
-    except Exception as e:
-        print("Erro ao abrir a imagem:", e)
+img = Image.open("image_rgb.png")
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Uso: python abrir_imagem_sistemas.py <caminho_da_imagem>")
-    else:
-        imagem = sys.argv[1]
-        abrir_imagem(imagem)
+img.show()
